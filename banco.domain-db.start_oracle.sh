3e9c6ec46b82bbb1026db24ea9fcebcc893095fa
Externo Tinet40 <externo.tinet40@consorcio.cl>
2023-11-02 10:22:50 -0500

    Add lines to create user on new DataBases

--- banco.domain-db.start_oracle.sh
+++ banco.domain-db.start_oracle.sh
@@ -3,5 +3,9 @@ docker run --name oracle \
         -e ORACLE_PASSWORD=oracle \
         -e APP_USER=banco \
         -e APP_USER_PASSWORD=oracle \
-        -e ORACLE_DATABASE=ORCL \
+        -e ORACLE_DATABASE=ORCL,AGENCIA,COLEGIO \
+        -v ~/repos-ucc/ucc-design-db/Scripts:/container-entrypoint-initdb.d \
         -d gvenzl/oracle-free
+sleep 30
+docker exec oracle createAppUser agencia oracle AGENCIA
+docker exec oracle createAppUser colegio oracle COLEGIO
